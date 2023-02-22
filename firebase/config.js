b8260229d5d76c632b9603bf71f9091ed006de31
joao vitor <vl351962@gmail.com>
2023-02-22 16:57:50 -0400

    adicionando todo o projeto

---
+++ firebase/config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBw-O3diii-99L0J2KjB_SVS4ycngD7ZkE",
+  authDomain: "mineblog-17b37.firebaseapp.com",
+  projectId: "mineblog-17b37",
+  storageBucket: "mineblog-17b37.appspot.com",
+  messagingSenderId: "931102795117",
+  appId: "1:931102795117:web:216d8da5c17b08429c6664"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db }; 

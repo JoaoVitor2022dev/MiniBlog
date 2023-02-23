518e49e5ab4dc5038790c4a177b342b11d31d6c9
joao vitor <vl351962@gmail.com>
2023-02-23 15:54:26 -0400

    fazendo o nav bar e o footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+// routers 
+import { BrowserRouter, Routes, Route , Navigate } from "react-router-dom";
+
+// css style
 import './App.css';
+
+// pages 
+import About from "./pages/About/About";
+import Home from "./pages/Home/Home";
+
+// componentes
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>mineBlog</h1>
+     <BrowserRouter>
+       <NavBar/>
+         <div className="container">
+             <Routes>
+                 <Route path="/" element={<Home/>} />
+                 <Route path="/about" element={<About/>}/>
+             </Routes>
+         </div>
+       <Footer/>  
+     </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/About/About.js
@@ -0,0 +1,13 @@
+// css 
+import style from "../About/About.module.css";
+
+
+const About = () => {
+  return (
+    <div>
+        <h1>About</h1>
+    </div>
+  )
+}
+
+export default About;
